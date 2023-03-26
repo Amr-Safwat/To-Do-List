512b6804de6480b54d612335649ad22be7e6db15
Amr Safwat <amrsafwat02@gmail.com>
2023-03-26 19:12:33 +0200

    Added A Some Modifiers

--- main.js
+++ main.js
@@ -1,52 +1,117 @@
 let input = document.querySelector('.input');
-let btn = document.querySelector('.add');
+let submit = document.querySelector('.add');
 let tasks = document.querySelector('.tasks');
-let task = document.querySelector('.task');
+let deleteAll = document.querySelector('.del-all');
 
-getFromLocalStorage();
+arrayOfTasks = [];
 
-btn.onclick = () => {
-	if (input.value !== '') {
-		addTask();
-		addInLocalStorage();
-		input.value = '';
-		input.style.border = 'none';
-	} else {
-		input.style.border = '1px solid red';
-		console.log('No Message here');
-	}
-};
-function addInLocalStorage() {
-	window.localStorage.setItem(input.value, input.value);
-}
-function getFromLocalStorage() {
-	for(let i=0;i< window.localStorage.length;i++) {
-		let task = document.createElement('div');
-		let span = document.createElement('span');
-		let txt = document.createTextNode(window.localStorage.key(i));
-		span.append('Delete');
-		task.className = 'task';
-		task.append(span);
-		task.append(txt);
-		tasks.appendChild(task);
-		span.onclick = () => {
-			task.remove();
-			removeFromLocal();
-			localStorage.removeItem(window.localStorage.key(i));
-		};
-	}
+// Check If Theres Tasks In Local Storage
+if(localStorage.tasks) {
+  arrayOfTasks = JSON.parse(localStorage.tasks);
 }
 
-function addTask() {
-	let task = document.createElement('div');
-	let span = document.createElement('span');
-	let txt = document.createTextNode(input.value);
-	span.append('Delete');
-	task.className = 'task';
-	task.append(span);
-	task.append(txt);
-	tasks.appendChild(task);
-	span.onclick = () => {
-		task.remove();
-	};
+// Trigger Function
+getDataFromLocalStorage();
+
+document.onkeyup = function (e) {
+  if(e.key === 'Enter') {
+    if(input.value !== '') {
+      addTask(input.value); //add task to array of tasks
+      input.value = ''; //empty input field
+		input.focus();
+    }
+  }
+}
+
+submit.onclick = function () {
+  if(input.value !== '') {
+    addTask(input.value); //add task to array of tasks
+    input.value = ''; //empty input field
+		input.focus();
+  }
+}
+ // Click On Delete All Butto
+ deleteAll.onclick = function () {
+  tasks.innerHTML = '';
+  localStorage.removeItem('tasks');
+ }
+// Click On Task Element
+tasks.addEventListener('click',(e)=>{
+  // Delete Button
+  if(e.target.classList.contains('del')) {
+    // Remove Task From Local Storage
+    deleteTaskWith(e.target.parentElement.getAttribute('data-id'));
+    // Remove Element From Page
+    e.target.parentElement.remove();
+  }
+  // Task Element
+  if(e.target.classList.contains('task')) {
+    // Toggle Completed For The Task
+    taskToggleStatusWith(e.target.getAttribute('data-id'));
+    // add class done
+    e.target.classList.toggle('done');
+  }
+})
+
+function addTask (taskName) {
+  const task = {
+    id: Date.now(),
+    title: taskName,
+    completed: false,
+  }
+  // push task to array
+  arrayOfTasks.push(task);
+  // add tasks to page with funtion
+  addElementToPageFrom(arrayOfTasks);
+  // Add Tasks To Local Storage
+  addDataToLocalStorageFrom(arrayOfTasks);
+}
+
+function addElementToPageFrom(arrayOfTasks) {
+// Empty the Tasks Div
+tasks.innerHTML = '';
+arrayOfTasks.forEach((task) => {
+  let div = document.createElement('div');
+  div.className = 'task';
+  // Check If Task Is Done
+  if(task.completed) {
+    div.className = 'task done';
+  }
+  div.setAttribute('data-id',task.id);
+  div.appendChild(document.createTextNode(task.title));
+  // Create Delete Button
+  let span = document.createElement('span');
+  span.className = 'del';
+  span.appendChild(document.createTextNode('delete'));
+  div.appendChild(span);
+  // add Task to Tasks Div
+  tasks.appendChild(div);
+});
+}
+
+function addDataToLocalStorageFrom(arrayOfTasks) {
+  window.localStorage.setItem('tasks',JSON.stringify(arrayOfTasks));
+}
+
+function getDataFromLocalStorage() {
+  let data = window.localStorage.getItem('tasks');
+  if(data) {
+    let tasks = JSON.parse(data);
+    addElementToPageFrom(tasks);
+  }
+}
+
+function deleteTaskWith(taskId) {
+  arrayOfTasks = arrayOfTasks.filter((task) => task.id != taskId);
+  addDataToLocalStorageFrom(arrayOfTasks);
+}
+
+function taskToggleStatusWith(taskId) {
+  for(let i=0;i< arrayOfTasks.length;i++) {
+    // console.log(arrayOfTasks[i].id);
+    if(arrayOfTasks[i].id == taskId) {
+      arrayOfTasks[i].completed === false?arrayOfTasks[i].completed = true:arrayOfTasks[i].completed = false;
+    }
+  }
+  addDataToLocalStorageFrom(arrayOfTasks);
 }
